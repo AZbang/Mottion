792b27d3778739ab2b94766ed643df7227a70b72
azbang <azbango5@gmail.com>
2017-12-15 15:44:05 +0300

    add music

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+require('pixi-sound');
 const Game = require('./game');
 
 PIXI.loader
   .add('blocks', 'assets/blocks.json')
+  .add('music', 'assets/music.mp3')
   .load((loader, resources) => {
-    console.log(PIXI.loader.resources);
+    // PIXI.sound.play('music');
     let game = new Game();
     window.game = game;
   });
