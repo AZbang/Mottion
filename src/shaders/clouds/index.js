c9ec8b067bfcb3bca1fc542467cc59eaed46ad63
azbang <azbango5@gmail.com>
2017-12-23 22:36:10 +0300

    init cloud filter

--- src/shaders/clouds/index.js
+++ src/shaders/clouds/index.js
@@ -1,7 +1,61 @@
-const shader = require('./shader.frag');
+const frag = require('./clouds.frag');
+const vert = require('../basic.vert');
 
 class CloudsFilter extends PIXI.Filter {
-  constructor() {
-    super();
+  constructor(options) {
+    super(vert(), frag(), {
+      cloudDensity: {
+        type: 'float',
+        value: 0.3
+      },
+      cloudHeight: {
+        type: 'float',
+        value: 0.3
+      },
+      speed: {
+        type: 'float',
+        value: 1.0
+      },
+      noisiness: {
+        type: 'float',
+        value: 0.5
+      }
+    });
+
+    Object.assign(this, {
+      cloudDensity: 0.3,
+      noisiness: 0.3,
+      speed: 1.0,
+      cloudHeight: 0.5
+    }, options);
+  }
+  set cloudDensity(v) {
+    this.uniforms.cloudDensity = v;
+  }
+  get cloudDensity() {
+    return this.uniforms.cloudDensity;
+  }
+
+  set noisiness(v) {
+    this.uniforms.noisiness = v;
+  }
+  get noisiness() {
+    return this.uniforms.noisiness;
+  }
+
+  set speed(v) {
+    this.uniforms.speed = v;
+  }
+  get speed() {
+    return this.uniforms.speed;
+  }
+
+  set cloudHeight(v) {
+    this.uniforms.cloudHeight = v;
+  }
+  get cloudHeight() {
+    return this.uniforms.cloudHeight;
   }
 }
+
+module.exports = CloudsFilter;
