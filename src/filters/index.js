dad06059725381dbf2600f62c4bcbe5615670bfc
Andrey Zhevlakov <azbangwtf@ya.ru>
2019-11-08 19:38:51 +0300

    build

--- src/filters/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.assign(PIXI.filters, {
-  CloudsFilter: require('./CloudsFilter'),
-  GrayscaleFilter: require('./GrayscaleFilter'),
-  NoiseBlurFilter: require('./NoiseBlurFilter'),
-  AlphaGradientFilter: require('./AlphaGradientFilter')
-});
--- src/scenes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'menu': require('./Menu'),
-  'playground': require('./Playground'),
-  'settings': require('./Settings'),
-}
